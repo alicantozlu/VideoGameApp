3f3b5f4121e923bb12887aed76805177ac34c94e
Ali Can Tozlu <alicantozlu@gmail.com>
2022-03-10 03:15:07 +0300

    GameList tasarimi duzenlendi, page controller duzenlenecek, searchbar duzenlenecek

--- VideoGamesApp/Cell/BottomCollectionViewCell.swift
+++ VideoGamesApp/Cell/BottomCollectionViewCell.swift
@@ -14,16 +14,41 @@ class BottomCollectionViewCell: UICollectionViewCell {
     @IBOutlet var gameImageView: ImageConfigure!
     @IBOutlet var ratingLbl: UILabel!
     @IBOutlet var innerImageView: ImageConfigure!
+    @IBOutlet var gameRatingStarLbl: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func configure(model: GameInfoModel){
-        gameNameLbl.text = model.name
-        releaseDateLbl.text = model.released
+        gameNameLbl.text = clearName(str: model.name!)
         gameImageView.loadImage(from: model.background_image!)
         innerImageView.loadImage(from: model.background_image!)
         ratingLbl.text = "\(model.rating!)"
+        gameRatingStarLbl.text = String(repeating: "👾", count: Int(model.rating!))
+        releaseDateLbl.text = changeDateFormat(str: model.released!)
+    }
+    
+    //tarih formatini yil-ay-gun formatindan gun.ay.yil formatina cevir
+    func changeDateFormat(str: String) -> (String) {
+        let data = Array(str)
+        let year = String(data[0..<4])
+        let month = String(data[5..<7])
+        let day = String(data[8..<10])
+        
+        return "\(day).\(month).\(year)"
+    }
+    
+    //Oyun isimlerindeki (tarih) ifadelerini kaldir
+    func clearName(str:String) -> (String){
+        var name:String = ""
+        for char in str{
+            if (char == "("){
+                break
+            }
+            name += String(char)
+        }
+        return name
     }
 }
+
--- VideoGamesApp/Controller/GameListViewController.swift
+++ VideoGamesApp/Controller/GameListViewController.swift
@@ -15,12 +15,14 @@ class GameListViewController: UIViewController {
     @IBOutlet var bottomCollectionView: UICollectionView!
     @IBOutlet var innerStackView: UIStackView!
     
+    //PageController kontrolu
     var currentPage = 0 {
         didSet{
             gamePageController.currentPage = currentPage
         }
     }
     
+    //Liste sonuna gelindiginde yeni oyunlarin eklenecegi liste
     var newGameList = [GameInfoModel](){
         didSet{
             print("BOTTOM:\(newGameList)")
@@ -30,10 +32,10 @@ class GameListViewController: UIViewController {
             DispatchQueue.main.async {
                 self.bottomCollectionView.reloadData()
             }
-            
         }
     }
     
+    //Ilk acilista oyunlarin eklenecegi model
     var gameList = [GameInfoModel](){
         didSet {
             // Array icerisine veri degisimi olursa tablolar guncellenmesi icin
@@ -73,7 +75,6 @@ class GameListViewController: UIViewController {
             }
         }
         
-        //let backgroundEmpty = EmptyView()
         bottomCollectionView.backgroundView = EmptyView()
         bottomCollectionView.backgroundView?.isHidden = true
         
@@ -86,17 +87,33 @@ class GameListViewController: UIViewController {
         guard let tabBar = tabBarController?.tabBar else { return }
         tabBar.isTranslucent = true
         tabBar.backgroundImage = UIImage()
-        tabBar.shadowImage = UIImage() // add this if you want remove tabBar separator
+        tabBar.shadowImage = UIImage()
         tabBar.barTintColor = .clear
         tabBar.backgroundColor = .clear // here is your tabBar color
         tabBar.layer.backgroundColor = UIColor.clear.cgColor
-        let blurEffect = UIBlurEffect(style: .light) // here you can change blur style
+        let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialLight) // here you can change blur style
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.frame = tabBar.bounds
         blurView.autoresizingMask = .flexibleWidth
         tabBar.insertSubview(blurView, at: 0)
         tabBar.tintColor = .white
         tabBar.unselectedItemTintColor = .darkGray
+        
+        /*
+        guard let tabBar = tabBarController?.tabBar else { return }
+        tabBar.isTranslucent = true
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
+        tabBar.barTintColor = .clear
+        tabBar.backgroundColor = .clear // here is your tabBar color
+        tabBar.layer.backgroundColor = UIColor.clear.cgColor
+        let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialLight) // here you can change blur style
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = tabBar.bounds
+        blurView.autoresizingMask = .flexibleWidth
+        tabBar.insertSubview(blurView, at: 0)
+        tabBar.tintColor = .white
+        tabBar.unselectedItemTintColor = .darkGray*/
     }
 }
 
